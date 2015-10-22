a62ae952fd8fed70e8c311bd15a22b32458450c8
Georg Wolfgang Winkler <winklerg@itp.phys.ethz.ch>
2015-10-22 14:07:18 +0200

    solution for ex05

---
+++ exercises/ex05/solutions/simpson/simpson.hpp
@@ -0,0 +1,36 @@
+/* Programming Techniques for Scientific Simulations, HS 2015
+ * Exercise 5.2
+ */
+
+#ifndef SIMPSON_HPP
+#define SIMPSON_HPP
+
+#include <cstdlib>
+#include <cassert>
+
+/// F needs to be a function or function object
+///     taking a single argument convertible from T
+///     with return value convertible to T
+/// T needs to be CopyConstructible, Assignable, 
+///     shall support arithmetic operations with double with
+///     result convertible to T with limited relative truncation errors
+template<class F, typename T>
+T integrate(const T a, const T b, const unsigned bins, const F& func)
+{
+  assert(bins > 0);
+  
+  const T dr = (b - a) / (2.*bins);
+  T I2(0), I4(0);
+  T pos = a;
+  
+  for(unsigned i = 0; i < bins; ++i) {
+    pos += dr;
+    I4 += func(pos);
+    pos += dr;
+    I2 += func(pos);
+  }
+  
+  return (func(a) + 2.*I2 + 4.*I4 - func(b)) * (dr/3.);
+}
+
+#endif
